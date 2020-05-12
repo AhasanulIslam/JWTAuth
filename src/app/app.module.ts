ee3f5de0e6ee47b336802e92c00eb94328ca21e4
AhasanulIslam <46520651+AhasanulIslam@users.noreply.github.com>
2020-05-12 10:41:47 +0600

    first auth

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// used to create fake backend
+// import { fakeBackendProvider } from './_helpers';
+
+import { AppComponent } from './app.component';
+import { appRoutingModule } from './app.routing';
+
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { ValueComponent } from './value/value.component';
+import { RegisterComponent } from './Register/Register.component';
+
+@NgModule({
+   imports: [
+      BrowserModule,
+      ReactiveFormsModule,
+      HttpClientModule,
+      appRoutingModule,
+      FormsModule
+   ],
+   declarations: [
+      AppComponent,
+      HomeComponent,
+      LoginComponent,
+      ValueComponent,
+      RegisterComponent
+   ],
+   providers: [
+      // provide
+   ],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
